3401999dcc3b494d82fb552f363ff15cfa43d78f
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-21 23:07:03 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+  attr_accessor :school, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(person, grade)
+    if @roster[grade].kind_of?(Array) != true
+      @roster[grade] = []
+    end
+    @roster[grade] << person
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    @roster[grade].each do |person|
+    end
+  end
+end
