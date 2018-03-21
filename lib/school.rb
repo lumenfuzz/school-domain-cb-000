8526418693179a8d84184a0835aae5f26ddbff8e
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-21 23:36:43 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  attr_accessor :school, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(person, grade)
+    if @roster[grade].kind_of?(Array) != true
+      @roster[grade] = []
+    end
+    @roster[grade] << person
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      students.sort!
+    end
+  end
+end
